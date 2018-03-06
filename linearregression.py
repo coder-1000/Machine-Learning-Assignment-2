cdabbce76ff2e051e1f0200823a0db21ee8f152a
De Almeida,  Paliharalalage <sahandealmeida@gmail.com>
2018-03-05 20:32:29 -0500

    jsut need to reset values and plot and i'll be done 1

--- linearregression.py
+++ linearregression.py
@@ -22,8 +22,9 @@ with np.load("notMNIST.npz") as data :
     testData, testTarget = Data[3600:], Target[3600:]
 
 PIXELS = 784;#image is 28 by 28
-NUM_ITERATIONS = 20000; #20000
+NUM_ITERATIONS = 200; #20000
 LEARNING_RATE = [0.005, 0.001, 0.0001];
+NUM_LR = 3;
 BATCH_SIZE = 500;
 NUM_BATCHES = 7;
 
@@ -40,6 +41,7 @@ y = tf.placeholder(tf.float64, [BATCH_SIZE, 1], name="targets");
 w = tf.Variable( np.ones((PIXELS,1), np.float64) ); #784 x 1 containing weights
 b = tf.Variable(tf.constant(1,tf.float64), tf.float64); #offset
 
+
 # (500x784) X (784x1) => (500x1) 
 yhat = tf.add(tf.matmul(x,w), b); 
 
@@ -47,30 +49,40 @@ yhat = tf.add(tf.matmul(x,w), b);
 #(500x1) => (1)
 loss = tf.reduce_mean(tf.reduce_sum((y - yhat)**2, 1))/2  ; #check dimensions
 
+lossValues = np.empty([NUM_LR, NUM_ITERATIONS], np.float64);
 
 #initialize global vars. need to do this to use variables
 initializer = tf.global_variables_initializer();
 
-print("this is the shape of trainX: ", trainX.shape);
-print("this is the shape of batchesX: ", batchesX.shape);
-print("this is the shape of batchesY: ", batchesY.shape);
+#print("this is the shape of trainX: ", trainX.shape);
+#print("this is the shape of batchesX: ", batchesX.shape);
+#print("this is the shape of batchesY: ", batchesY.shape);
 
-print("this is the shape of a batchX: ", batchesX[2].shape);
-print("this is the type of a batchX: ", batchesX.dtype);
+#print("this is the shape of a batchX: ", batchesX[2].shape);
+#print("this is the type of a batchX: ", batchesX.dtype);
 
-print("this is the shape of a batchY: ", batchesY.shape);
-print("this is the type of a batchY: ", batchesY.dtype);
+#print("this is the shape of a batchY: ", batchesY.shape);
+#print("this is the type of a batchY: ", batchesY.dtype);
 
 with tf.Session() as sess:
    sess.run(initializer);  
+   lrNum = 0;
    for learningRate in LEARNING_RATE:
+        
+        #reset the values for each learning rate so one doesn't have an unfair advantage over another
+            
+        print(sess.run(tf.reduce_mean(w) ) );    
+        #set up optimizer
         sgdOptimizer = tf.train.GradientDescentOptimizer(learningRate).minimize(loss);
+        
+        #descend the gradient 
         for i in range(NUM_ITERATIONS*NUM_BATCHES):
             sess.run(sgdOptimizer, feed_dict={x: batchesX[i%NUM_BATCHES] , y: batchesY[i%NUM_BATCHES]});       
-            if( i % 10000 == 0):
-                print("iteration: ", i);
-                     
-        #print(sess.run(w));
-        print(sess.run(loss, feed_dict={x: batchesX[i%NUM_BATCHES] , y: batchesY[i%NUM_BATCHES]} ) )
-
-
+            if( i % NUM_BATCHES == 0):
+                lossValues[lrNum][i/NUM_BATCHES] = sess.run(loss, feed_dict={x: batchesX[i%NUM_BATCHES] , y: batchesY[i%NUM_BATCHES]} );
+            #if(i % 10000 == 0):    
+                #print("iteration: ", i);
+        
+        print(sess.run(tf.reduce_mean(w) ) );        
+        #print(sess.run(loss, feed_dict={x: batchesX[i%NUM_BATCHES] , y: batchesY[i%NUM_BATCHES]} ) )
+        lrNum+=1;
